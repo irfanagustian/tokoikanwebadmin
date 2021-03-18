3a14f6b387c6d7f33caaa153055435045ee8fb95
ipan <irfanagustian0608@gmail.com>
2021-03-18 12:06:50 +0700

    commit kedua

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if($user){
+            if(password_verify($request->password, $user->password)){
+                return response()->json([
+                    'sucsess' => 1,
+                    'message' => 'Selamat Datang '.$user->name,
+                    'user' => $user
+                ]);
+            }
+            return $this->error('Password Salah');
+        }
+        return $this->error('Email tidak ditemukan');
+
+    }
+
+    public function register(Request $request){
+        $validasi = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required|min:8'
+        ]);
+        if($validasi->fails()){
+            $val = $validasi->errors()->all();
+            return $this->error($val[0]);
+        }
+
+        $user = User::create(array_merge($request->all(), [
+            'password' => bcrypt($request->password)
+        ]));
+
+        if($user){
+            return response()->json([
+                'success' => 1,
+                'message' => 'Selamat Registrasi Anda Berhasil',
+                'user' => $user
+            ]);
+        }
+        return $this->error('Registrasi Gagal');
+    }
+
+    public function error($message){
+        return response()->json([
+            'sucsess' => 0,
+            'message' => $message
+        ]);
+    }
+}
